9a0cbd1dbf0f09a7ad501cb157537c165a748dae
冉丹 <RDan456@163.com>
2020-07-01 22:03:18 +0800

    Update ziplist.h

--- src/ziplist.h
+++ src/ziplist.h
@@ -1,53 +1,23 @@
-/*
- * Copyright (c) 2009-2012, Pieter Noordhuis <pcnoordhuis at gmail dot com>
- * Copyright (c) 2009-2012, Salvatore Sanfilippo <antirez at gmail dot com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of Redis nor the names of its contributors may be used
- *     to endorse or promote products derived from this software without
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef _ZIPLIST_H
 #define _ZIPLIST_H
 
-#define ZIPLIST_HEAD 0
+#define ZIPLIST_HEAD 0  
 #define ZIPLIST_TAIL 1
 
-unsigned char *ziplistNew(void);
-unsigned char *ziplistMerge(unsigned char **first, unsigned char **second);
-unsigned char *ziplistPush(unsigned char *zl, unsigned char *s, unsigned int slen, int where);
-unsigned char *ziplistIndex(unsigned char *zl, int index);
-unsigned char *ziplistNext(unsigned char *zl, unsigned char *p);
-unsigned char *ziplistPrev(unsigned char *zl, unsigned char *p);
-unsigned int ziplistGet(unsigned char *p, unsigned char **sval, unsigned int *slen, long long *lval);
-unsigned char *ziplistInsert(unsigned char *zl, unsigned char *p, unsigned char *s, unsigned int slen);
-unsigned char *ziplistDelete(unsigned char *zl, unsigned char **p);
-unsigned char *ziplistDeleteRange(unsigned char *zl, int index, unsigned int num);
-unsigned int ziplistCompare(unsigned char *p, unsigned char *s, unsigned int slen);
-unsigned char *ziplistFind(unsigned char *p, unsigned char *vstr, unsigned int vlen, unsigned int skip);
-unsigned int ziplistLen(unsigned char *zl);
-size_t ziplistBlobLen(unsigned char *zl);
+unsigned char *ziplistNew(void); ///创建一个空的压缩表
+unsigned char *ziplistMerge(unsigned char **first, unsigned char **second); ///合并两个压缩表
+unsigned char *ziplistPush(unsigned char *zl, unsigned char *s, unsigned int slen, int where); ///在压缩表的头或者尾部插入一个节点
+unsigned char *ziplistIndex(unsigned char *zl, int index); ///查找index下标处的节点
+unsigned char *ziplistNext(unsigned char *zl, unsigned char *p);///获取p指向节点的下一个节点
+unsigned char *ziplistPrev(unsigned char *zl, unsigned char *p); ///获取p指向节点的前驱节点
+unsigned int ziplistGet(unsigned char *p, unsigned char **sval, unsigned int *slen, long long *lval); ///获取p所指向的节点信息
+unsigned char *ziplistInsert(unsigned char *zl, unsigned char *p, unsigned char *s, unsigned int slen); ///在p所指的位置插入节点，如果p在压缩表，则插入p前面
+unsigned char *ziplistDelete(unsigned char *zl, unsigned char **p); //删除p所指的位置的节点
+unsigned char *ziplistDeleteRange(unsigned char *zl, int index, unsigned int num); ///删除从index处开始的num个节点
+unsigned int ziplistCompare(unsigned char *p, unsigned char *s, unsigned int slen);  ///比较p所指的节点和s
+unsigned char *ziplistFind(unsigned char *p, unsigned char *vstr, unsigned int vlen, unsigned int skip); ///压缩表中寻找和vstr值相等的节点
+unsigned int ziplistLen(unsigned char *zl); ///获取压缩表的长度
+size_t ziplistBlobLen(unsigned char *zl); ///获取压缩表的二进制长度
 void ziplistRepr(unsigned char *zl);
 
 #ifdef REDIS_TEST
